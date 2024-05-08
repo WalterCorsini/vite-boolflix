3dc2dc1c03e5aba03661269f9effe98531fbf9c0
WalterCorsini <corsini.walter@gmail.com>
2024-05-08 12:47:26 +0200

    add style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,13 +3,72 @@
 </script>
 
 <template>
-<div>
-    Header
-</div>
+    <div class="header">
+        <div class="container">
+            <span>BOOLFLIX</span>
+            <div class="search-container">
+                <label for="search">trova un film</label>
+                <div>
+                    <input id="search" type="text" placeholder="ricerca..">
+                    <button>cerca</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/variable" as *;
 @use "../style/partials/mixin" as *;
 
+.header {
+    background-color: $black;
+    padding: 10px;
+    
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 80%;
+        margin: 0 auto;
+        
+        span {
+            color: $red;
+            font-weight: 800;
+            font-size: 30px;
+        }
+        .search-container {
+            display: flex;
+            flex-direction: column;
+            padding: 10px;
+            border: 2px solid $red;
+            border-radius: 10px;
+
+            label {
+                color: $red;
+                margin-left: 20px;
+            }
+
+            input {
+                border: 1px solid black;
+                border-right: 0;
+                border-top-left-radius: 10px;
+                border-bottom-left-radius: 10px;
+                outline: 0;
+                padding-left: 10px;
+                padding: 1px 10px;
+            }
+
+            button {
+                // border: 1px solid red;
+                border: 0;
+                padding: 1px 10px;
+                background-color: red;
+                color: $white;
+                border-top-right-radius: 10px;
+                border-bottom-right-radius: 10px;
+            }
+        }
+    }
+}
 </style>
