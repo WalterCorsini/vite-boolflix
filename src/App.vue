9c6b3e9bf0fabdbf4cce997e269e4857a4a67ec7
WalterCorsini <corsini.walter@gmail.com>
2024-05-10 18:57:53 +0200

    whit select call api for film and series

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@ import AppMain from "./components/AppMain.vue";
 
 export default {
     components: {
-    AppHeader,
-    AppMain,
-  },
-    data(){
-        return{
+        AppHeader,
+        AppMain,
+    },
+    data() {
+        return {
             store,
         }
     },
@@ -21,41 +21,67 @@ export default {
         this.searchType();
     },
 
-    methods:{
-        SearchFilmSeries(){
-                const paramsobj = {
-                    api_key: this.store.apiKey,
-                    query: this.store.searchQuery,
-                }
-            const CallMovie = axios.get("https://api.themoviedb.org/3/search/movie",{
-                params: paramsobj});
-            const CallSerie = axios.get("https://api.themoviedb.org/3/search/tv",{
-                params: paramsobj});
+    methods: {
+        SearchFilmSeries() {
+            const paramsobj = {
+                api_key: this.store.apiKey,
+                query: this.store.searchQuery,
+            }
+            const CallMovie = axios.get("https://api.themoviedb.org/3/search/movie", {
+                params: paramsobj
+            });
+            const CallSerie = axios.get("https://api.themoviedb.org/3/search/tv", {
+                params: paramsobj
+            });
 
             axios
-                .all([CallMovie,CallSerie])
+                .all([CallMovie, CallSerie])
                 .then((resp) => {
                     this.store.arrayFilm = resp[0].data.results;
                     this.store.arraySerie = resp[1].data.results;
                 })
-                this.store.searchQuery = "";
-            },
-        searchType(){
+            this.store.searchQuery = "";
+        },
+        searchType() {
             console.log(this.store.currentOption);
-            const paramsobj = {  api_key: this.store.apiKey  }
-            if(this.store.currentOption === "default"){
+            const paramsobj = { api_key: this.store.apiKey }
+            if (this.store.currentOption === "default") {
                 paramsobj.query = this.store.randomWord[this.store.randomNumber];
-                axios.get("https://api.themoviedb.org/3/search/movie",{
-                params: paramsobj})
-                .then((resp) => {
-                    this.store.arrayFilm = resp.data.results;
+                const CallMovie = axios.get("https://api.themoviedb.org/3/search/movie", {
+                    params: paramsobj
                 });
-            } else{
-                axios.get(`https://api.themoviedb.org/3/movie/${this.store.currentOption}`,{
-                params: paramsobj})
-                .then((resp) => {
-                    this.store.arrayFilm = resp.data.results;
+                const CallSerie = axios.get("https://api.themoviedb.org/3/search/tv", {
+                    params: paramsobj
                 });
+                axios
+                    .all([CallMovie, CallSerie])
+                    .then((resp) => {
+                        this.store.arrayFilm = resp[0].data.results;
+                        this.store.arraySerie = resp[1].data.results;
+                    })
+            } else if (this.store.currentOption === "popular" || this.store.currentOption === "top_rated") {
+                //   chiamata doppia per serie e per film
+                const CallMovie = axios.get(`https://api.themoviedb.org/3/movie/${this.store.currentOption}`, {
+                    params: paramsobj
+                });
+                const CallSerie = axios.get(`https://api.themoviedb.org/3/tv/${this.store.currentOption}`, {
+                    params: paramsobj
+                });
+
+                axios
+                    .all([CallMovie, CallSerie])
+                    .then((resp) => {
+                        this.store.arrayFilm = resp[0].data.results;
+                        this.store.arraySerie = resp[1].data.results;
+                    })
+            } else {
+                //  chiamata solo per film
+                axios.get(`https://api.themoviedb.org/3/movie/${this.store.currentOption}`, {
+                    params: paramsobj
+                })
+                    .then((resp) => {
+                        this.store.arrayFilm = resp.data.results;
+                    });
             }
         }
     }
@@ -64,10 +90,8 @@ export default {
 
 <template>
 
-<AppHeader @searchFilm="SearchFilmSeries" @changeOption="searchType" />
-<AppMain />
+    <AppHeader @searchFilm="SearchFilmSeries" @changeOption="searchType" />
+    <AppMain />
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
